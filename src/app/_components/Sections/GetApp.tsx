3fee23ac8fe30bdcc6adfda3afbbe90f1b9c9a91
Joao Lara <joaovdelara@gmail.com>
2026-07-20 16:58:23 -0300

    fix getapp

--- src/app/_components/Sections/GetApp.tsx
+++ src/app/_components/Sections/GetApp.tsx
@@ -3,10 +3,10 @@ import { infos } from "@/src/app/_data/data";
 export default function GetApp() {
   return (
     <section>
-      <article className="container-1 flex flex-col md:flex-row justify-between items-center p-6">
+      <article className="container-1 flex flex-col md:flex-row justify-between md:items-center px-6">
         <h2 className="">Baixe o nosso app:</h2>
 
-        <div className="flex flex-col md:flex-row gap-4 p-3">
+        <div className="flex md:flex-row gap-4 pt-3 md:p-0">
           <button className="btn">ANDROID</button>
           <button className="btn">APPLE iOS</button>
         </div>
