c658c9cef3600a3666bc9833a590ab057bf595b8
Joao Lara <joaovdelara@gmail.com>
2026-07-22 18:14:23 -0300

    fix wpp

--- src/app/_components/WppButton.tsx
+++ src/app/_components/WppButton.tsx
@@ -6,7 +6,7 @@ import { infos } from "@/src/app/_data/data";
 export default function WhatsAppButton() {
   return (
     <a
-      href={infos.wppLink}
+      href={infos.social.whatsapp}
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-4 right-4 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition flex items-center justify-center z-50"
