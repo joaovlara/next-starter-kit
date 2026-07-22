a9e1ed017e99e55d0500f3cc4bc93471851a0a72
Joao Lara <joaovdelara@gmail.com>
2026-07-22 16:59:24 -0300

    dinamic header page

--- src/app/_components/HeaderPage.tsx
+++ src/app/_components/HeaderPage.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 
-export default function HeaderPage() {
+interface HeaderPageProps {
+  title?: string;
+  text?: string;
+}
+
+const DEFAULT_TITLE = "$h1-title";
+const DEFAULT_TEXT =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam perspiciatis expedita cupiditate, optio ipsam minus, nihil vel repudiandae quod repellat. Quos soluta consequuntur quaerat expedita, ea ratione omnis itaque?";
+
+export default function HeaderPage({
+  title = DEFAULT_TITLE,
+  text = DEFAULT_TEXT,
+}: HeaderPageProps) {
   return (
     <section className="p-6">
-      <article className="container-1 rounded-br-[100] bg-gray-200 p-6">
+      <article className="container-1 rounded-br-[100] bg-gray-200 p-3">
         <div className="p-6 space-y-3">
-          <h1>Titulo h1 da página</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam
-            perspiciatis expedita cupiditate, optio ipsam minus, nihil vel
-            repudiandae quod repellat. Quos soluta consequuntur quaerat
-            expedita, ea ratione omnis itaque?
-          </p>
+          <h1>{title}</h1>
+          <p>{text}</p>
         </div>
       </article>
     </section>
