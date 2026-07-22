b157b48ed4b8065a27f90b78ac5b73e2ec0d0d3d
Joao Lara <joaovdelara@gmail.com>
2026-07-22 16:20:16 -0300

    create servicos page

--- src/app/servicos/page.tsx
+++ src/app/servicos/page.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import HeaderPage from "../_components/HeaderPage";
 import CTA from "../_components/Sections/CTA";
+import { infos, servicos } from "@/src/app/_data/data";
 
-export default function Servicos() {
+export default function ServicosPage() {
   return (
     <main>
       <HeaderPage />
       <section className="px-6">
-        <article className="container-1 bg-gray-200">
-          Serviços
+        <article className="container-1 grid md:grid-cols-3 gap-3 p-0">
+          {servicos.map((item) => (
+            <div
+              id="usar um id para cada para a URL"
+              key={item.title}
+              className="bg-gray-200 w-full h-30 md:h-75"
+            >
+              {item.title} Card que abre e fecha
+            </div>
+          ))}
         </article>
-        <CTA />
       </section>
+      <CTA />
     </main>
   );
 }
