abf51d8ad646800a4740cd133ac79ffb8eadec94
Joao Lara <joaovdelara@gmail.com>
2026-08-03 16:49:39 -0300

    fix wpp button size

--- src/app/_components/WppButton.tsx
+++ src/app/_components/WppButton.tsx
@@ -27,9 +27,9 @@ export default function WhatsAppButton() {
       }}
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
-      className="fixed bottom-6 right-6 p-6 text-olive-300 rounded-full flex items-center justify-center z-50 bg-primary hover:bg-primary-2 shadow-lg transition-colors duration-300"
+      className="fixed bottom-6 right-6 md:p-6 p-3 text-olive-300 rounded-full flex items-center justify-center z-50 bg-primary hover:bg-primary-2 shadow-lg transition-colors duration-300"
     >
-      <FaWhatsapp size={48} />
+      <FaWhatsapp size={45} />
     </motion.a>
   );
 }
